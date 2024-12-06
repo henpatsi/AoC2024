87eb2e30d50a564f16882d9c2108ccb7c52284dc
Henri Pätsi <patsihenri@gmail.com>
2024-12-06 19:31:18 +0200

    day4 pt1 fixed

--- 4/pt1/main.py
+++ 4/pt1/main.py
@@ -1,39 +1,36 @@
 XMAS = "XMAS"
 
-def find_xmases(lines, x, y, i):
-    xmas_count = 0
-    if i >= len(XMAS) or y >= len(lines) or x >= len(lines[y]):
-        return 0
-    char = lines[y][x]
-    if char != XMAS[i]:
-        return 0
-    else:
-        print(char, x, y)
-    if i == len(XMAS) - 1:
-        return 1
-    xmas_count += find_xmases(lines, x + 1, y, i+1)
-    xmas_count += find_xmases(lines, x - 1, y, i+1)
-    xmas_count += find_xmases(lines, x, y + 1, i+1)
-    xmas_count += find_xmases(lines, x, y - 1, i+1)
-    xmas_count += find_xmases(lines, x + 1, y + 1, i+1)
-    xmas_count += find_xmases(lines, x - 1, y + 1, i+1)
-    xmas_count += find_xmases(lines, x + 1, y - 1, i+1)
-    xmas_count += find_xmases(lines, x - 1, y - 1, i+1)
-    return xmas_count
+def check_xmas(lines, x, y, x_dir, y_dir):
+    for i in range(len(XMAS)):
+        cur_x = x + x_dir * i
+        cur_y = y + y_dir * i
+        if cur_y >= len(lines) or cur_x >= len(lines[y]) or cur_y < 0 or cur_x < 0:
+            return 0
+        if lines[cur_y][cur_x] != XMAS[i]:
+            return 0
+    return 1
+
 
 def main():
     xmas_count = 0
 
     lines = []
 
-    input_file = open("input2", "r")
+    input_file = open("input", "r")
 
     for line in input_file:
         lines.append(line.strip())
 
     for y in range(len(lines)):
         for x in range(len(lines[y])):
-            xmas_count += find_xmases(lines, x, y, 0)
+            xmas_count += check_xmas(lines, x, y, 1, 0)
+            xmas_count += check_xmas(lines, x, y, 0, 1)
+            xmas_count += check_xmas(lines, x, y, -1, 0)
+            xmas_count += check_xmas(lines, x, y, 0, -1)
+            xmas_count += check_xmas(lines, x, y, 1, 1)
+            xmas_count += check_xmas(lines, x, y, 1, -1)
+            xmas_count += check_xmas(lines, x, y, -1, 1)
+            xmas_count += check_xmas(lines, x, y, -1, -1)
 
     print(xmas_count)
 
