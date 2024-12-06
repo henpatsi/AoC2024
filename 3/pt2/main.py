5eb20437c91141332565f6ae8236db5b6b4b7818
Henri Pätsi <patsihenri@gmail.com>
2024-12-06 19:13:32 +0200

    day4 pt1 wrong interpretation

---
+++ 3/pt2/main.py
@@ -0,0 +1,30 @@
+import re
+
+REGEX_PATTERN = "mul\([0-9]{1,3},[0-9]{1,3}\)"
+
+g_mul_enabled = True
+
+def get_line_muls(line):
+    line_mul_sum = 0
+
+    muls = re.search(REGEX_PATTERN, line)
+    while muls != None:
+        nums = line[muls.start() + 4:muls.end() - 1].split(",")
+        line_mul_sum += int(nums[0]) * int(nums[1])
+        line = line[muls.end() - 1:]
+        muls = re.search(REGEX_PATTERN, line)
+
+    return line_mul_sum
+
+def main():
+    mul_sum = 0
+
+    input_file = open("input", "r")
+
+    for line in input_file:
+        mul_sum += get_line_muls(line)
+
+    print(mul_sum)
+
+if __name__ == '__main__':
+    main()
---
+++ 4/pt1/main.py
@@ -0,0 +1,41 @@
+XMAS = "XMAS"
+
+def find_xmases(lines, x, y, i):
+    xmas_count = 0
+    if i >= len(XMAS) or y >= len(lines) or x >= len(lines[y]):
+        return 0
+    char = lines[y][x]
+    if char != XMAS[i]:
+        return 0
+    else:
+        print(char, x, y)
+    if i == len(XMAS) - 1:
+        return 1
+    xmas_count += find_xmases(lines, x + 1, y, i+1)
+    xmas_count += find_xmases(lines, x - 1, y, i+1)
+    xmas_count += find_xmases(lines, x, y + 1, i+1)
+    xmas_count += find_xmases(lines, x, y - 1, i+1)
+    xmas_count += find_xmases(lines, x + 1, y + 1, i+1)
+    xmas_count += find_xmases(lines, x - 1, y + 1, i+1)
+    xmas_count += find_xmases(lines, x + 1, y - 1, i+1)
+    xmas_count += find_xmases(lines, x - 1, y - 1, i+1)
+    return xmas_count
+
+def main():
+    xmas_count = 0
+
+    lines = []
+
+    input_file = open("input2", "r")
+
+    for line in input_file:
+        lines.append(line.strip())
+
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            xmas_count += find_xmases(lines, x, y, 0)
+
+    print(xmas_count)
+
+if __name__ == '__main__':
+    main()
